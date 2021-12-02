0d99db89898a6bc5dd3c57f7df0185b456156290
Elvan H <elvanhd@gmail.com>
2021-12-02 21:07:50 +0700

    outputting lists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('John');
+  const [title, setTitle] = useState('My name is Giovanni Giorgio');
+  const [events, setEvents] = useState([
+    {
+      id: 1,
+      title: 'National Pollution Control Day'
+    },
+    {
+      id: 2,
+      title: 'International Day for the abolition of slavery'
+    },
+    {
+      id: 3,
+      title: 'International Volunteer Day'
+    }
+  ]);
 
   const handleClick = () => {
-    console.log(name);
-    setName('James');
-    console.log(name);
+    setTitle('But everybody calls me Giorgio');
+    setEvents((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: prevState.length + 1,
+          title: "New Year's Eve"
+        }
+      ];
+    });
   };
-
-  useEffect(() => {
-    console.log(name);
-  }, [name]);
 
   return (
     <div className='App'>
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change Name</button>
+      <h1>{title}</h1>
+      <button onClick={handleClick}>Change Title</button>
+      <ul>
+        {events.map((event) => (
+          <div key={event.id}>
+            <h2>
+              #{event.id} {event.title}
+            </h2>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
